17a1e819e1f989ca233708671b9c60ca2a9f4d4f
CL0824 <leargieepogi@gmail.com>
2026-05-04 13:57:50 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const showConfirm = (message, onConfirm) => {
 };
 // --- Google Sheets Database URL ---
 // PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL
-const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzsc4moE0x57-sXn6lUjZ1fGBkW3wnAbPno4jRMy7uCKKVGbUlt4dUbQRraIGi_aNkM/exec';
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbyblEIeDL0FEwDF5r0h66aOZIUUfBMSdCt0tmNOzSROek77G3fVr5e7VUsW7W4hwtgT/exec';
 
 const Storage = {
   /**
